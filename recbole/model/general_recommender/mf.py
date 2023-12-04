83527724f8e75741f95d6cf8a36bce9df829c567
Paitesanshi <45314239+Paitesanshi@users.noreply.github.com>
2023-12-04 14:44:10 +0800

    FEAT: convert mf to pairwise

--- recbole/model/general_recommender/mf.py
+++ recbole/model/general_recommender/mf.py
@@ -15,6 +15,7 @@ from genericpath import exists
 import torch
 import torch.nn as nn
 import numpy as np
+from recbole.model.loss import BPRLoss
 from recbole.model.init import xavier_normal_initialization
 from recbole.utils import InputType
 from recbole.model.abstract_recommender import GeneralRecommender
@@ -24,7 +25,7 @@ class MF(GeneralRecommender):
     r"""
     MF model
     """
-    input_type = InputType.POINTWISE
+    # input_type = InputType.POINTWISE
 
     def __init__(self, config, dataset):
         super(MF, self).__init__(config, dataset)
@@ -32,6 +33,7 @@ class MF(GeneralRecommender):
         self.task = config['task']
         self.LABEL = config['LABEL_FIELD']
         self.RATING = config['RATING_FIELD']
+        self.loss_type=config['loss_type']
         # self.TIME = config['TIME_FIELD']
         # load parameters info
         self.embedding_size = config['embedding_size']
@@ -44,12 +46,16 @@ class MF(GeneralRecommender):
         self.b_u = nn.Embedding(self.n_users, 1)
         self.b_i = nn.Embedding(self.n_items, 1)
         self.b = nn.Parameter(torch.Tensor(1))
-        if self.task == 'ps':
-            self.loss = nn.BCELoss(reduce='mean')
-            self.sigmoid = nn.Sigmoid()
+        if self.loss_type=="BPR":
+            self.loss = BPRLoss()
+
         else:
-            self.loss = nn.MSELoss(reduce=False)
-            self.sigmoid = None
+            if self.task == 'ps':
+                self.loss = nn.BCELoss(reduce='mean')
+                self.sigmoid = nn.Sigmoid()
+            else:
+                self.loss = nn.MSELoss(reduce=False)
+                self.sigmoid = None
 
         # parameters initialization
         self.apply(xavier_normal_initialization)
@@ -83,16 +89,29 @@ class MF(GeneralRecommender):
 
     def calculate_loss(self, interaction, weight=None):
         user = interaction[self.USER_ID]
-        item = interaction[self.ITEM_ID]
-        # time = interaction[self.TIME]
-        if self.task == 'ps':
-            label = interaction[self.LABEL]
-        else:
-            label = interaction[self.RATING]
+       
+        if self.loss_type=="BPR":
+            pos_item = interaction[self.ITEM_ID]
+            neg_item = interaction[self.NEG_ITEM_ID]
 
-        # output = self.forward(user, item, time)
-        output = self.forward(user, item)
-        loss = self.loss(output, label)
+            user_e, pos_e = self.forward(user, pos_item)
+            neg_e = self.get_item_embedding(neg_item)
+            pos_item_score, neg_item_score = torch.mul(user_e, pos_e).sum(dim=1), torch.mul(
+                user_e, neg_e
+            ).sum(dim=1)
+            loss = self.loss(pos_item_score, neg_item_score)
+            print(loss)
+        else: 
+            item = interaction[self.ITEM_ID]
+            # time = interaction[self.TIME]
+            if self.task == 'ps':
+                label = interaction[self.LABEL]
+            else:
+                label = interaction[self.RATING]
+
+            # output = self.forward(user, item, time)
+            output = self.forward(user, item)
+            loss = self.loss(output, label)
         if weight != None:
             loss *= weight
         loss = torch.sum(loss) / len(loss)
